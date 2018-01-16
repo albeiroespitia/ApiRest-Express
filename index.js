2292c296827e7b8e59da2cd930d724e7c4026d17
Albeiro <albeiroespitiasierra@gmail.com>
2018-01-16 04:40:44 -0500

    Creando el servidor con express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-holaa
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/hola',(req,res)=>{
+    res.end('hola');
+})
+
+app.listen(port, ()=>{
+    console.log(`Escuchando en el puerto ${port}...`)
+})
