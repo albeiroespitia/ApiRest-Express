7186124831da96cafb591925aaab44c072d57359
Albeiro <albeiroespitiasierra@gmail.com>
2018-01-18 02:43:09 -0500

    Agregando Json Web Tokens

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     port: process.env.PORT || 3000,
-    db: process.env.MONGODB || 'mongodb://localhost:27017/products'
+    db: process.env.MONGODB || 'mongodb://localhost:27017/products',
+    SECRET_TOKEN : 'miclavedetokens'
 }
